33e7c5924dd190ed6dd222b694afe34805e81267
Efrelle Joyce Ducyao <efrellejoyced@gmail.com>
2024-09-08 14:02:28 +0700

    Completed Variables Section

---
+++ app.js
@@ -0,0 +1,34 @@
+console.log('Hello, World!');
+let myname = "Efelle Joyce Ducyao";
+console.log('My name is ' + myname);
+
+console.log('USING VARIABLE');
+let name = "Efrelle Joyce Ducyao";
+let age = "19";
+console.log("Hello! I am" + name + "," + age+ " years old.");
+
+console.log("USING NUMBERS");
+const x = 4;
+const y = 5;
+
+const sum = x + y;
+console.log(sum);
+
+console.log('USING BOOLEANS');
+let c = true;
+let d = false;
+console.log("I am learning programming" ,c);
+console.log("I will give up." ,d);
+
+console.log('COMPARIS ON OPERATORS');
+let e = 15;
+let f = 19;
+
+let g = e > f;
+console.log("Is e more than f?"+ g);
+let h=e>f;
+console.log("Is e less than f?"+ h);
+let i=e==f;
+console.log("Is e equal to f?"+ i);
+let j=e!=f
+console.log("Is e not equal to f?"+ j);
